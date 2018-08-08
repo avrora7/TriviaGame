9a1cec42ab3a4984e56150c34c0cc9bf3a2b7ee3
Elizabeth <avrora7@yahoo.com>
2018-08-07 22:58:55 -0700

    working code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,40 +1,76 @@
-const correctAnswers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const correctAnswers = ["2", "3", "4", "1", "1", "1", "1", "1", "1"];
 let countdown = 0;
-var numCorrAnsw = 3;
-var numIncorrAnsw = 5;
+let numCorrAnsw = 3;
+let numIncorrAnsw = 5;
+let countdownInterval = null;
 
-function display() {
+// display results of the questions/answers on the last screen
+function displayResults() {
     $('#correct').html(numCorrAnsw);
     $('#incorrect').html(numIncorrAnsw);
     $('#unanswered').html(correctAnswers.length - numCorrAnsw - numIncorrAnsw);
 }
 
+// display countdown, decrement countdown, handle Done button when time runs out
 function displayTimeRem() {
+    $('#timeRem').html(--countdown);
+    if (countdown === 0) {
+        handleDone();
+    }
+}
+
+// handle Done button either when Done button is pressed or time runs out - 
+// hide 1st and 2nd screens, show last screen, dispaly questions/answers results, clear countdown
+function handleDone() {
+    matchQandA();
+    $('#intro').hide()
+    $('#questions').hide()
+    $('#summary').show()
+    displayResults();
+    clearInterval(countdownInterval);
+}
+
+function matchQandA() {
+    for (i = 0; i < correctAnswers.length; i++) {
+        let selector = "input[name=q_" + (i+1) + "]:checked"; 
+      
+        let nextQuestionValue = $(selector).val();
+       
+        if (nextQuestionValue === correctAnswers[i]) {
+            numCorrAnsw++
+        }
+        else if (nextQuestionValue != null) {
+            numIncorrAnsw++
+        }
+        else {
+            
+        }
+    }
+}
+
+// reset game either on start or restart -
+// reset correct and incorect answers counter, re/start coutdown,
+function resetGame() {
+    $("form")[0].reset();
+    countdown = 120;
+    numCorrAnsw = numIncorrAnsw = 0;
+    countdownInterval = setInterval(displayTimeRem, 1000);
     $('#timeRem').html(countdown);
 }
 
-function handleDone() {
-        $('#intro').hide()
-        $('#questions').hide()
-        $('#summary').show()
-        display();
-}
-
-
+// attach click handlers to buttons start, done, restart
 $(document).ready(function () {
-
     $('#start, #restart').on('click', function () {
         $('#intro').hide()
         $('#questions').show()
         $('#summary').hide()
         displayTimeRem();
-        setTimeout(handleDone, 10*1000);
+        resetGame();
     })
-    $('#done').on('click', function () {
-       handleDone();
-    })
-    
 
+    $('#done').on('click', function () {
+        handleDone();
+    })
 });
 
 
